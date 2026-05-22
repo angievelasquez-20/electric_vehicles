bb1c9346c0f8b2518ef23a3d0321d0b494797b1e
tiomaspro <tomasfelipeninoroa901@gmail.com>
2026-05-21 22:58:08 -0500

    Addition of model k means to the project

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request
 import logic
 from regression import train_regression_model
 import os
+import kmeans as Clustering
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/index')
 def index():
@@ -40,6 +41,38 @@ def regression():
 
     except Exception as e:
         return f"Linear regression error: {str(e)}", 500
+
+
+@app.route('/K_means', methods=['GET', 'POST'])
+def K_means():
+
+    results = None
+    summaryClusters = None
+    centers = None
+    plot_url = None
+
+    if request.method == 'POST':
+
+        k = int(request.form['k'])
+
+        info = Clustering.applyClusteringKmeans(k)
+
+        results = info["results"]
+        summaryClusters = info["summaryClusters"]
+        centers = info["centers"]
+
+        Clustering.generate_plot(results, centers)
+
+        plot_url = 'kmeans_plot.png'
+
+    return render_template(
+        'k_means.html',
+        results=results,
+        summaryClusters=summaryClusters,
+        centers=centers,
+        plot_url=plot_url
+    )
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
